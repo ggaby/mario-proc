80aa6028d2bbf6b470577a681fc0df457ed682c0
perezoscluis@gmail.com <utnso@utnso.(none)>
2013-07-01 15:48:06 -0300

    Se corrige bug de fragmentacion en cuando se usa una particion existente.

--- src/Memoria/memoria.c
+++ src/Memoria/memoria.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "memoria.h"
 
@@ -23,7 +25,7 @@ int guardar_datos(t_fragmento* guardar_aca, t_memoria segmento, char id,
 		char* contenido);
 t_fragmento* fragmento_create(char id, int tamanio, int inicio, bool ocupado);
 void fragmento_destroy(t_fragmento* self);
-
+void crear_particion_vacia_en_posicion(t_memoria segmento, char id, int tamanio, bool ocupado, int posicion);
 
 t_memoria crear_memoria(int tamanio) {
 	tamanioSegmento = tamanio;
@@ -74,26 +76,46 @@ t_list* particiones(t_memoria segmento) {
 		return elem->particion;
 	}
 
-	if (libre > 0) {
-		crear_particion_nueva(segmento, '0', libre, "", true);
+	bool _esMayor(t_particion* elem1, t_particion* elem2){
+		return elem1->inicio < elem2->inicio;
 	}
 	t_list *listaPantalla = list_map(listaParticiones, (void*) get_particion);
+	list_sort(listaPantalla, (void*) _esMayor);
+	if (libre > 0) {
+		t_particion* ultimaParticion = malloc(sizeof(t_particion));
+		ultimaParticion->id='0';
+		ultimaParticion->inicio=tamanioSegmento-libre;
+		ultimaParticion->tamanio=libre;
+		ultimaParticion->libre=true;
+		char* lugar_del_segmento = segmento + (tamanioSegmento-libre);
+		ultimaParticion->dato = lugar_del_segmento;
+		list_add(listaPantalla, ultimaParticion);
+	}
 	return listaPantalla;
 }
 
 int crear_particion_nueva(t_memoria segmento, char id, int tamanio,
-		char* contenido, bool ocupado) {
+		char* contenido, bool vacio) {
 
-	t_fragmento* nuevo = fragmento_create(id, tamanio, last_pos_segmento, ocupado);
+	t_fragmento* nuevo = fragmento_create(id, tamanio, last_pos_segmento, vacio);
 	char* lugar_del_segmento = segmento + last_pos_segmento;
-	strcpy(lugar_del_segmento, contenido);
 	nuevo->particion->dato = lugar_del_segmento;
-	memcpy(nuevo->particion->dato, lugar_del_segmento, tamanio);
+	memcpy(nuevo->particion->dato, contenido, tamanio);
 	last_pos_segmento += tamanio;
 	actual_index_particiones = list_add(listaParticiones, nuevo);
 	nuevo->index = actual_index_particiones;
 	libre -= tamanio;
 	return 1; //Todo joya
+}
+
+void crear_particion_vacia_en_posicion(t_memoria segmento, char id, int tamanio, bool vacio, int posicion) {
+
+	t_fragmento* nuevo = fragmento_create(id, tamanio, posicion, vacio);
+	char* lugar_del_segmento = segmento + posicion;
+	nuevo->particion->dato = lugar_del_segmento;
+	last_pos_segmento += tamanio;
+	actual_index_particiones = list_add(listaParticiones, nuevo);
+	nuevo->index = actual_index_particiones;
 }
 
 t_fragmento* buscar_la_primera_que_entre(int tamanio) {
@@ -125,20 +147,27 @@ t_fragmento* buscar_elemento(char id) {
 
 int guardar_datos(t_fragmento* guardar_aca, t_memoria segmento, char id,
 		char* contenido) {
-	guardar_aca->particion->id = id;
-	guardar_aca->particion->libre = false;
-
-	char* lugar_del_segmento = segmento + guardar_aca->particion->inicio;
-	strcpy(lugar_del_segmento, contenido);
+	t_fragmento* elem = fragmento_create(id, strlen(contenido), guardar_aca->particion->inicio, false);
+	elem->particion->dato= segmento + guardar_aca->particion->inicio;
+	memcpy(elem->particion->dato, contenido, strlen(contenido));
+	elem->index = guardar_aca->index;
+	int viejoTamanio = guardar_aca->particion->tamanio-elem->particion->tamanio;
+	int nuevoInicio = guardar_aca->particion->inicio +strlen(contenido);
+	char idParticionVacia = tolower(guardar_aca->particion->id);
 	actual_index_particiones = guardar_aca->index;
+	list_replace_and_destroy_element(listaParticiones, guardar_aca->index, elem,(void*) fragmento_destroy);
+	last_pos_segmento = guardar_aca->particion->inicio +strlen(contenido);
+	if(viejoTamanio !=0){
+		crear_particion_vacia_en_posicion(segmento, idParticionVacia, viejoTamanio, true, nuevoInicio);
+	}
 	return 1;
 }
 
-t_fragmento* fragmento_create(char id, int tamanio, int inicio, bool ocupado) {
+t_fragmento* fragmento_create(char id, int tamanio, int inicio, bool vacio) {
 	t_particion* particion = malloc(sizeof(t_particion));
 	particion->id = id;
 	particion->tamanio = tamanio;
-	particion->libre = ocupado;
+	particion->libre = vacio;
 	particion->inicio = inicio;
 
 	t_fragmento* new = malloc(sizeof(t_fragmento));
@@ -148,10 +177,10 @@ t_fragmento* fragmento_create(char id, int tamanio, int inicio, bool ocupado) {
 }
 
 void fragmento_destroy(t_fragmento* self) {
-	free(self->particion->dato);
 	free(self->particion);
 	free(self);
 }
+
 
 
 
