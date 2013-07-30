6964c6094bf64875adc05d777d8c058d86129f55
DemianFuentes <fuentes.demian@gmail.com>
2013-07-30 02:53:44 -0300

    fix verificador_deadlock terminado

--- src/Nivel/VerificadorDeadlock/verificador_deadlock.c
+++ src/Nivel/VerificadorDeadlock/verificador_deadlock.c
@@ -3,7 +3,6 @@
 #include "../../common/list.h"
 #include <commons/string.h>
 
-bool hay_espera_circular(t_list* personajes_bloqueados);
 void loguear_deadlock_detectado(nivel_t_nivel* nivel, t_list* personajes_bloqueados);
 
 void* verificador_deadlock(void* level) {
@@ -22,11 +21,17 @@ void* verificador_deadlock(void* level) {
 		sleep(nivel->tiempoChequeoDeadlock);
 
 		t_list* recursos_disponibles = clonar_recursos(nivel);
-		t_list* personajes_bloqueados = nivel_get_personajes_bloqueados(nivel);
+		t_list* personajes_bloqueados = clonar_personajes(nivel);
 
 		bool hay_deadlock = false;
 
-		bool verificar_personaje_en_deadlock(nivel_t_personaje* personaje) {
+		bool personaje_puede_ejecutar(nivel_t_personaje* personaje) {
+			if(personaje->simbolo_recurso_esperado ==  NULL){
+				liberar_recursos_del_personaje(personaje, recursos_disponibles);
+				hay_deadlock = false;
+				return true;
+			}
+
 			bool es_el_recurso(t_recurso* recurso) {
 				return recurso->simbolo
 						== personaje->simbolo_recurso_esperado[0];
@@ -44,16 +49,14 @@ void* verificador_deadlock(void* level) {
 			return false;
 		}
 
-		while (!hay_deadlock && list_size(personajes_bloqueados) > 1) { //1 solo bloqueado nunca puede estar en deadlock
+		while (!hay_deadlock && !list_is_empty(personajes_bloqueados)) {
 			hay_deadlock = true;
 			my_list_remove_and_destroy_by_condition(personajes_bloqueados,
-					(void*) verificar_personaje_en_deadlock,
+					(void*) personaje_puede_ejecutar,
 					(void*) nivel_destroy_personaje);
 		}
 
-		if (hay_deadlock && hay_espera_circular(personajes_bloqueados)) {
-			//TODO En caso de que varios esten esperando el recurso que tenga tomado un unico personaje que puede terminar su ejecucion esto tira deadlock pero no es cierto... hay que verificar espera circular entre los procesos restantes?
-
+		if (hay_deadlock) {
 			loguear_deadlock_detectado(nivel, personajes_bloqueados);
 
 			if(nivel->recovery){
@@ -74,11 +77,6 @@ void* verificador_deadlock(void* level) {
 	}
 
 	return (void*) EXIT_SUCCESS;
-}
-
-bool hay_espera_circular(t_list* personajes_bloqueados){
-	//TODO falto de ideas para ver como hacer esto :S
-	return true;
 }
 
 void loguear_deadlock_detectado(nivel_t_nivel* nivel, t_list* personajes_bloqueados){
@@ -115,34 +113,33 @@ void liberar_recursos_del_personaje(nivel_t_personaje* personaje,
 
 	list_iterate(personaje->recursos_asignados, (void *) liberar_recurso);
 
-	free(personaje->simbolo_recurso_esperado);
-	personaje->simbolo_recurso_esperado = NULL;
 }
 
-t_list* nivel_get_personajes_bloqueados(nivel_t_nivel* nivel) {
-	t_list* personajes_bloqueados = list_create();
+t_list* clonar_personajes(nivel_t_nivel* nivel) {
+//	t_list* personajes_bloqueados = list_create();
 
-	void clonar_personaje_bloqueado(nivel_t_personaje* personaje) {
-		if (personaje->simbolo_recurso_esperado != NULL ) {
+	nivel_t_personaje* clonar_personaje(nivel_t_personaje* personaje) {
+		nivel_t_personaje* new = malloc(sizeof(nivel_t_personaje));
+		new->id = personaje->id;
 
-			nivel_t_personaje* new = malloc(sizeof(nivel_t_personaje));
-
-			new->id = personaje->id;
-
+		if(personaje->simbolo_recurso_esperado == NULL){
+			new->simbolo_recurso_esperado = NULL;
+		}
+		else{
 			new->simbolo_recurso_esperado = string_duplicate(
 					personaje->simbolo_recurso_esperado);
-
-			new->recursos_asignados = my_list_clone_and_clone_elements(
-					personaje->recursos_asignados, (void*) recurso_clone);
-
-			new->posicion = NULL;
-
-			list_add(personajes_bloqueados, new);
 		}
+
+		new->recursos_asignados = my_list_clone_and_clone_elements(
+				personaje->recursos_asignados, (void*) recurso_clone);
+		new->posicion = NULL;
+
+		return new;
 	}
 
-	list_iterate(nivel->personajes, (void*) clonar_personaje_bloqueado);
-	return personajes_bloqueados;
+//	list_iterate(nivel->personajes, (void*) clonar_personaje_bloqueado);
+//	return personajes_bloqueados;
+	return my_list_clone_and_clone_elements(nivel->personajes, (void*) clonar_personaje);
 }
 
 t_list* clonar_recursos(nivel_t_nivel* nivel) {
--- src/Nivel/VerificadorDeadlock/verificador_deadlock.h
+++ src/Nivel/VerificadorDeadlock/verificador_deadlock.h
@@ -7,7 +7,7 @@
 void* verificador_deadlock(void* nivel);
 
 t_list* clonar_recursos(nivel_t_nivel* nivel);
-t_list* nivel_get_personajes_bloqueados(nivel_t_nivel* nivel);
+t_list* clonar_personajes(nivel_t_nivel* nivel);
 void liberar_recursos_del_personaje(nivel_t_personaje* personaje,
 		t_list* recursos_disponibles);
 
