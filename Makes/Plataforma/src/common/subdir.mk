6732fc18f980c01b90c59bed6a768ef7f9044e3d
Oscar Perez <perezoscluis@gmail.com>
2013-08-01 17:35:54 -0300

    Se corrige error en el path de gcc

--- Makes/Plataforma/src/common/subdir.mk
+++ Makes/Plataforma/src/common/subdir.mk
@@ -32,7 +32,7 @@ C_DEPS += \
 src/common/%.o: ../../src/common/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/workspace/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"/home/utnso/so-commons-library/src" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
